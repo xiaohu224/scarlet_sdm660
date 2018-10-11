15dd76548dd902deaa09398ae49db8b07ba2477c
Kees Cook <keescook@chromium.org>
2018-10-10 17:18:18 -0700

    UPSTREAM: vmlinux.lds.h: Avoid copy/paste of security_init section
    Avoid copy/paste by defining SECURITY_INIT in terms of SECURITY_INITCALL.
    
    Change-Id: Ic66bf945c7951aa3ae72381f5264ee4510d574d9
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Casey Schaufler <casey@schaufler-ca.com>
    Reviewed-by: James Morris <james.morris@microsoft.com>
    Reviewed-by: John Johansen <john.johansen@canonical.com>
    Signed-off-by: James Morris <james.morris@microsoft.com>

--- include/asm-generic/vmlinux.lds.h
+++ include/asm-generic/vmlinux.lds.h
@@ -839,6 +839,17 @@
 		KEEP(*(.con_initcall.init))				\
 		__con_initcall_end = .;
 
+#define SECURITY_INITCALL						\
+		__security_initcall_start = .;				\
+		KEEP(*(.security_initcall.init))			\
+		__security_initcall_end = .;
+
+/* Older linker script style for security init. */
+#define SECURITY_INIT							\
+	.security_initcall.init : AT(ADDR(.security_initcall.init) - LOAD_OFFSET) { \
+		SECURITY_INITCALL					\
+	}
+
 #ifdef CONFIG_BLK_DEV_INITRD
 #define INIT_RAM_FS							\
 	. = ALIGN(4);							\
