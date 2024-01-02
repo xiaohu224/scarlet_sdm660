af44e5e0459bc32e09dcafcfd7021eca9c68e259
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2024-01-02 17:23:45 +0600

    CLO: Merge tag `LA.UM.12.2.1.r1-02400-sdm660.0` of kernel/msm-4.19
    • CodeLinaro msm-4.19 Kernel Source: https://git.codelinaro.org/clo/la/kernel/msm-4.19
    • Branch/Tag: LA.UM.12.2.1.r1-02400-sdm660.0
    • Contains:
      - CLO tags: LA.UM.12.2.1.r1-01800-sdm660.0, LA.UM.12.2.1.r1-02000-sdm660.0 & LA.UM.12.2.1.r1-02400-sdm660.0
    
    • Commit log (33 commits):
      vidc_3x: Return EBUSY if mbpf check fails
      msm:vidc_3x: reject sessions based on max mbpf
      msm:vidc_3x: reject sessions based on max mbpf
      BACKPORT: blk-mq: fix is_flush_rq
      BACKPORT: blk-mq: clearing flush request reference in tags->rqs[]
      BACKPORT: blk-mq: clear stale request in tags->rq[] before freeing one request pool
      BACKPORT: blk-mq: grab rq->refcount before calling ->fn in blk_mq_tagset_busy_iter
      ARM: msm: Add board config support for 32 bit qcm2150
      soc: qcom: socinfo: Add supprot for QCM2150 QRD soc-id
      dwc3-msm-core: Remove usage of DWC31_LINK_GDBGLTSSM with POR
      defconfig: kona: Fix for SonyDualSenseEdge cts failures
      usb: dwc3-msm-core: Set pipectl susphy in conndone interrupt
      bus: mhi: fix potential out-of-bound access
      bus: mhi: fix potential out-of-bound access
      usb: dwc3: gadget: Bail out in pullup if soft reset timeout happens
      iommu: Fix missing return check of arm_lpae_init_pte
      msm: kgsl: Limit the syncpoint count for AUX commands
      msm: kgsl: Prevent wrap around during user address mapping
      msm: kgsl: Limit the syncpoint count for AUX commands
      msm: kgsl: Prevent wrap around during user address mapping
      ...
    
    Change-Id: I7ee656cb6e12fabec4553cef91b4520ca2ba076f
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -526,9 +526,6 @@ inline void __blk_mq_end_request(struct request *rq, blk_status_t error)
 		blk_stat_add(rq, now);
 	}
 
-	if (rq->internal_tag != -1)
-		blk_mq_sched_completed_request(rq, now);
-
 	blk_account_io_done(rq, now);
 
 	if (rq->end_io) {
@@ -565,6 +562,8 @@ static void __blk_mq_complete_request(struct request *rq)
 
 	if (!blk_mq_mark_complete(rq))
 		return;
+	if (rq->internal_tag != -1)
+		blk_mq_sched_completed_request(rq);
 
 	if (!test_bit(QUEUE_FLAG_SAME_COMP, &rq->q->queue_flags)) {
 		rq->q->softirq_done_fn(rq);
@@ -813,6 +812,14 @@ static bool blk_mq_req_expired(struct request *rq, unsigned long *next)
 	return false;
 }
 
+void blk_mq_put_rq_ref(struct request *rq)
+{
+	if (is_flush_rq(rq))
+		rq->end_io(rq, 0);
+	else if (refcount_dec_and_test(&rq->ref))
+		__blk_mq_free_request(rq);
+}
+
 static void blk_mq_check_expired(struct blk_mq_hw_ctx *hctx,
 		struct request *rq, void *priv, bool reserved)
 {
@@ -845,11 +852,9 @@ static void blk_mq_check_expired(struct blk_mq_hw_ctx *hctx,
 	 */
 	if (blk_mq_req_expired(rq, next))
 		blk_mq_rq_timed_out(rq, reserved);
+	blk_mq_put_rq_ref(rq);
+	return;
 
-	if (is_flush_rq(rq, hctx))
-		rq->end_io(rq, 0);
-	else if (refcount_dec_and_test(&rq->ref))
-		__blk_mq_free_request(rq);
 }
 
 static void blk_mq_timeout_work(struct work_struct *work)
@@ -1147,7 +1152,6 @@ bool blk_mq_dispatch_rq_list(struct request_queue *q, struct list_head *list,
 	bool no_tag = false;
 	int errors, queued;
 	blk_status_t ret = BLK_STS_OK;
-	bool no_budget_avail = false;
 
 	if (list_empty(list))
 		return false;
@@ -1164,11 +1168,8 @@ bool blk_mq_dispatch_rq_list(struct request_queue *q, struct list_head *list,
 		rq = list_first_entry(list, struct request, queuelist);
 
 		hctx = blk_mq_map_queue(rq->q, rq->mq_ctx->cpu);
-		if (!got_budget && !blk_mq_get_dispatch_budget(hctx)) {
-			blk_mq_put_driver_tag(rq);
-			no_budget_avail = true;
+		if (!got_budget && !blk_mq_get_dispatch_budget(hctx))
 			break;
-		}
 
 		if (!blk_mq_get_driver_tag(rq)) {
 			/*
@@ -1264,15 +1265,13 @@ bool blk_mq_dispatch_rq_list(struct request_queue *q, struct list_head *list,
 		 *
 		 * If driver returns BLK_STS_RESOURCE and SCHED_RESTART
 		 * bit is set, run queue after a delay to avoid IO stalls
-		 * that could otherwise occur if the queue is idle.  We'll do
-		 * similar if we couldn't get budget and SCHED_RESTART is set.
+		 * that could otherwise occur if the queue is idle.
 		 */
 		needs_restart = blk_mq_sched_needs_restart(hctx);
 		if (!needs_restart ||
 		    (no_tag && list_empty_careful(&hctx->dispatch_wait.entry)))
 			blk_mq_run_hw_queue(hctx, true);
-		else if (needs_restart && (ret == BLK_STS_RESOURCE ||
-					   no_budget_avail))
+		else if (needs_restart && (ret == BLK_STS_RESOURCE))
 			blk_mq_delay_run_hw_queue(hctx, BLK_MQ_RESOURCE_DELAY);
 
 		blk_mq_update_dispatch_busy(hctx, true);
@@ -1454,25 +1453,6 @@ void blk_mq_run_hw_queues(struct request_queue *q, bool async)
 	}
 }
 EXPORT_SYMBOL(blk_mq_run_hw_queues);
-
-/**
- * blk_mq_delay_run_hw_queues - Run all hardware queues asynchronously.
- * @q: Pointer to the request queue to run.
- * @msecs: Microseconds of delay to wait before running the queues.
- */
-void blk_mq_delay_run_hw_queues(struct request_queue *q, unsigned long msecs)
-{
-	struct blk_mq_hw_ctx *hctx;
-	int i;
-
-	queue_for_each_hw_ctx(q, hctx, i) {
-		if (blk_mq_hctx_stopped(hctx))
-			continue;
-
-		blk_mq_delay_run_hw_queue(hctx, msecs);
-	}
-}
-EXPORT_SYMBOL(blk_mq_delay_run_hw_queues);
 
 /**
  * blk_mq_queue_stopped() - check whether one or more hctxs have been stopped
@@ -1870,7 +1850,6 @@ static void blk_mq_clear_rq_mapping(struct blk_mq_tag_set *set,
 		struct blk_mq_tags *tags, unsigned int hctx_idx)
 {
 	struct blk_mq_tags *drv_tags = set->tags[hctx_idx];
-	struct ext_blk_mq_tags *drv_etags;
 	struct page *page;
 	unsigned long flags;
 
@@ -1896,9 +1875,8 @@ static void blk_mq_clear_rq_mapping(struct blk_mq_tag_set *set,
 	 * Request reference is cleared and it is guaranteed to be observed
 	 * after the ->lock is released.
 	 */
-	drv_etags = container_of(drv_tags, struct ext_blk_mq_tags, tags);
-	spin_lock_irqsave(&drv_etags->lock, flags);
-	spin_unlock_irqrestore(&drv_etags->lock, flags);
+	spin_lock_irqsave(&drv_tags->lock, flags);
+	spin_unlock_irqrestore(&drv_tags->lock, flags);
 }
 
 static blk_qc_t blk_mq_make_request(struct request_queue *q, struct bio *bio)
@@ -2224,18 +2202,51 @@ static void blk_mq_remove_cpuhp(struct blk_mq_hw_ctx *hctx)
 					    &hctx->cpuhp_dead);
 }
 
+/*
+ * Before freeing hw queue, clearing the flush request reference in
+ * tags->rqs[] for avoiding potential UAF.
+ */
+static void blk_mq_clear_flush_rq_mapping(struct blk_mq_tags *tags,
+		unsigned int queue_depth, struct request *flush_rq)
+{
+	int i;
+	unsigned long flags;
+
+	/* The hw queue may not be mapped yet */
+	if (!tags)
+		return;
+
+	WARN_ON_ONCE(refcount_read(&flush_rq->ref) != 0);
+
+	for (i = 0; i < queue_depth; i++)
+		 cmpxchg(&tags->rqs[i], flush_rq, NULL);
+
+	/*
+	 * Wait until all pending iteration is done.
+	 *
+	 * Request reference is cleared and it is guaranteed to be observed
+	 * after the ->lock is released.
+	 */
+	spin_lock_irqsave(&tags->lock, flags);
+	spin_unlock_irqrestore(&tags->lock, flags);
+}
+
 /* hctx->ctxs will be freed in queue's release handler */
 static void blk_mq_exit_hctx(struct request_queue *q,
 		struct blk_mq_tag_set *set,
 		struct blk_mq_hw_ctx *hctx, unsigned int hctx_idx)
 {
+	struct request *flush_rq = hctx->fq->flush_rq;
+
 	blk_mq_debugfs_unregister_hctx(hctx);
 
 	if (blk_mq_hw_queue_mapped(hctx))
 		blk_mq_tag_idle(hctx);
 
+	blk_mq_clear_flush_rq_mapping(set->tags[hctx_idx],
+			set->queue_depth, flush_rq);
 	if (set->ops->exit_request)
-		set->ops->exit_request(set, hctx->fq->flush_rq, hctx_idx);
+		set->ops->exit_request(set, flush_rq, hctx_idx);
 
 	if (set->ops->exit_hctx)
 		set->ops->exit_hctx(hctx, hctx_idx);
@@ -3203,9 +3214,10 @@ static bool blk_mq_poll_hybrid_sleep(struct request_queue *q,
 	kt = nsecs;
 
 	mode = HRTIMER_MODE_REL;
-	hrtimer_init_sleeper_on_stack(&hs, CLOCK_MONOTONIC, mode);
+	hrtimer_init_on_stack(&hs.timer, CLOCK_MONOTONIC, mode);
 	hrtimer_set_expires(&hs.timer, kt);
 
+	hrtimer_init_sleeper(&hs, current);
 	do {
 		if (blk_mq_rq_state(rq) == MQ_RQ_COMPLETE)
 			break;
