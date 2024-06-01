737b96a14f95b866b55c371766d081091647863d
LKDenchin <69336052+LKDenchin@users.noreply.github.com>
2024-06-01 21:14:15 +0800

    Update exec.c

--- fs/exec.c
+++ fs/exec.c
@@ -1751,6 +1751,11 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+// KernelSU hook
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			       void *envp, int *flags);
+
+
 /*
  * sys_execve() executes a new program.
  */
@@ -1763,6 +1768,8 @@ static int __do_execve_file(int fd, struct filename *filename,
 	struct linux_binprm bprm;
 	struct files_struct *displaced;
 	int retval;
+	
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);  // call KSU hook first
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
