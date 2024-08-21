c313ed1254473a409643dfaed468a4eed0dac59b
WenHao2130 <WenHao2130@outlook.com>
2024-08-21 13:28:32 +0800

    fs: hooking is only performed when `CONFIG_KSU` is defined

--- fs/stat.c
+++ fs/stat.c
@@ -164,9 +164,10 @@ EXPORT_SYMBOL(vfs_statx_fd);
  * 0 will be returned on success, and a -ve error code if unsuccessful.
  */
  
- // KernelSU hook
+#ifdef CONFIG_KSU
+// KernelSU hook
 extern int ksu_handle_stat(int *dfd, const char __user **filename_user);
-
+#endif
 
 int vfs_statx(int dfd, const char __user *filename, int flags,
 	      struct kstat *stat, u32 request_mask)
@@ -174,9 +175,10 @@ int vfs_statx(int dfd, const char __user *filename, int flags,
 	struct path path;
 	int error = -EINVAL;
 	unsigned int lookup_flags = LOOKUP_FOLLOW | LOOKUP_AUTOMOUNT;
-	
+
+#ifdef CONFIG_KSU
 	ksu_handle_stat(&dfd, &filename); // call KSU hook first
-	
+#endif
 
 	if ((flags & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
 		       AT_EMPTY_PATH | KSTAT_QUERY_FLAGS)) != 0)
