076cd1c070dc93c017b28b1349944733e302b480
WenHao2130 <WenHao2130@outlook.com>
2025-04-04 12:00:54 +0800

    kernel: use `#ifdef CONFIG_KSU_MANUAL_HOOK` instead of `#ifdef CONFIG_KSU`
    Signed-off-by: WenHao2130 <WenHao2130@outlook.com>

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -379,7 +379,7 @@ static int input_get_disposition(struct input_dev *dev,
 }
 
 // KernelSU hook
-#ifdef CONFIG_KSU
+#ifdef CONFIG_KSU_MANUAL_HOOK
 extern bool ksu_input_hook __read_mostly;
 extern int ksu_handle_input_handle_event(unsigned int *type, unsigned int *code, int *value);
 #endif
@@ -390,7 +390,7 @@ static void input_handle_event(struct input_dev *dev,
 	int disposition = input_get_disposition(dev, type, code, &value);
 
 	// KernelSU hook
-#ifdef CONFIG_KSU
+#ifdef CONFIG_KSU_MANUAL_HOOK
 	if (unlikely(ksu_input_hook))
 		ksu_handle_input_handle_event(&type, &code, &value);
 #endif
--- fs/exec.c
+++ fs/exec.c
@@ -1925,7 +1925,7 @@ out_ret:
 }
 
 // KernelSU hook
-#ifdef CONFIG_KSU
+#ifdef CONFIG_KSU_MANUAL_HOOK
 extern bool ksu_execveat_hook __read_mostly;
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			void *envp, int *flags);
@@ -1939,7 +1939,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 			      int flags)
 {
 	// KernelSU hook
-#ifdef CONFIG_KSU
+#ifdef CONFIG_KSU_MANUAL_HOOK
 	if (unlikely(ksu_execveat_hook))
 		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 	else
