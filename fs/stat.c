c63440afa67cfa5f6a3522a82ce8cd2fa207f87d
WenHao2130 <WenHao2130@outlook.com>
2025-04-04 12:00:54 +0800

    kernel: use `#ifdef CONFIG_KSU_MANUAL_HOOK` instead of `#ifdef CONFIG_KSU`
    Signed-off-by: WenHao2130 <WenHao2130@outlook.com>

--- fs/stat.c
+++ fs/stat.c
@@ -149,7 +149,7 @@ int vfs_statx_fd(unsigned int fd, struct kstat *stat,
 EXPORT_SYMBOL(vfs_statx_fd);
 
 // KernelSU hook
-#ifdef CONFIG_KSU
+#ifdef CONFIG_KSU_MANUAL_HOOK
 extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
 #endif
 
@@ -176,7 +176,8 @@ int vfs_statx(int dfd, const char __user *filename, int flags,
 	int error = -EINVAL;
 	unsigned int lookup_flags = LOOKUP_FOLLOW | LOOKUP_AUTOMOUNT;
 
-#ifdef CONFIG_KSU
+	// KernelSU hook
+#ifdef CONFIG_KSU_MANUAL_HOOK
 	ksu_handle_stat(&dfd, &filename, &flags);
 #endif
 
