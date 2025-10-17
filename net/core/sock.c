c64d3194b4d6debe4d88d7d25081d0f99b523aa2
xiaohu224 <xiaohuluobo2240@outlook.com>
2025-10-18 00:28:32 +0800

    Revert "Update sock.c"
    This reverts commit 258225f51283338ebbe00efe65dbc563cedcf252.

--- net/core/sock.c
+++ net/core/sock.c
@@ -2854,7 +2854,7 @@ void sk_stop_timer_sync(struct sock *sk, struct timer_list *timer)
 }
 EXPORT_SYMBOL(sk_stop_timer_sync);
 
-void sock_init_data_uid(struct socket *sock, struct sock *sk, kuid_t uid)
+void sock_init_data(struct socket *sock, struct sock *sk)
 {
 	sk_init_common(sk);
 	sk->sk_send_head	=	NULL;
