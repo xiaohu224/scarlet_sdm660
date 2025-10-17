9ff40d9b228dbe9a60133ced004e2c1c688d0f4d
xiaohu224 <xiaohuluobo2240@outlook.com>
2025-10-18 00:55:40 +0800

    Revert "rcu: Apply RCU-bh QSes to RCU-sched and RCU-preempt when safe"
    This reverts commit 9f854778043284d8a506db44fb85bf848f90dcc0.

--- kernel/rcu/tree.h
+++ kernel/rcu/tree.h
@@ -405,7 +405,6 @@ static const char *tp_rcu_varname __used __tracepoint_string = rcu_name;
 
 /* Forward declarations for tree_plugin.h */
 static void rcu_bootup_announce(void);
-static void rcu_preempt_qs(void);
 static void rcu_preempt_note_context_switch(bool preempt);
 static int rcu_preempt_blocked_readers_cgp(struct rcu_node *rnp);
 #ifdef CONFIG_HOTPLUG_CPU
