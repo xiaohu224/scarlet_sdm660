f7939688727cd8dad04f8a9e33131fa87c4e5d32
xiaohu224 <xiaohuluobo2240@outlook.com>
2025-10-18 00:44:54 +0800

    Update tcp.h

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -2038,7 +2038,7 @@ static inline s64 tcp_rto_delta_us(const struct sock *sk)
 {
 	const struct sk_buff *skb = tcp_rtx_queue_head(sk);
 	u32 rto = inet_csk(sk)->icsk_rto;
-	u64 rto_time_stamp_us = skb->skb_mstamp + jiffies_to_usecs(rto);
+	u64 rto_time_stamp_us = skb->skb_mstamp_ns + jiffies_to_usecs(rto);
 
 	return rto_time_stamp_us - tcp_sk(sk)->tcp_mstamp;
 }
